418f5f19cc87f372ee5846c9e837f86e5c200cb2
unknown <satpal61106@gmail.com>
2026-04-22 10:04:42 +0530

    code updated

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 MONGO_URI = process.env.MONGO_URI
 const connectDb = async() =>{
    try {
-    await mongoose.connect(`${MONGO_URI}`)
+    await mongoose.connect(MONGO_URI)
     console.log("database connected successfully")
    } catch (error) {
     console.log(`database connecting error ${error}`)
